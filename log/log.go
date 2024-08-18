f69d21edde01ddf85db2797bc7040602e8291ade
nhatphuoc <vonhatphuoc32@gmail.com>
2024-08-19 05:46:26 +0700

    fix createDB, Received_Dev_Info

--- log/log.go
+++ log/log.go
@@ -9,6 +9,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CreateLog(db *sql.DB) func(*gin.Context) {
+	return func(c *gin.Context){
+		var gal Log
+		if err := c.ShouldBind(&gal); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		exec := fmt.Sprintf(`insert into %s (url,time_taken)
+		value (?,?)`, Log{}.TableName())
+		_,err := db.Exec(exec, gal.Url, gal.Time)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK,true)
+	}
+}
+
 func ListLog(db *sql.DB) func(*gin.Context) {
 	return func(c *gin.Context) {
 		query := fmt.Sprintf(`SELECT * from %s`, Log{}.TableName())
